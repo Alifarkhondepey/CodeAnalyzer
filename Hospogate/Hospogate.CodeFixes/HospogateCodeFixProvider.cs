a841ac5d56aa08fd13c4a57509ff718fb190bb5c
ali <AliFarkhondepey@TotallyTech.co>
2024-02-13 15:47:44 +0400

    Add project files.

---
+++ Hospogate/Hospogate.CodeFixes/HospogateCodeFixProvider.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hospogate.CodeAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(HospogateCodeFixProvider))]
+    [Shared]
+    public class HospogateCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(HospogateAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var node = root.FindNode(diagnosticSpan);
+
+            switch (node)
+            {
+                case NamespaceDeclarationSyntax namespaceDeclaration:
+                    context.RegisterCodeFix(
+                        CodeAction.Create("Fix namespace naming", c => FixNamespaceAsync(context.Document, namespaceDeclaration, c)),
+                        diagnostic);
+                    break;
+                case ClassDeclarationSyntax classDeclaration:
+                    context.RegisterCodeFix(
+                        CodeAction.Create("Fix class naming", c => FixClassAsync(context.Document, classDeclaration, c)),
+                        diagnostic);
+                    break;
+                case InterfaceDeclarationSyntax interfaceDeclaration:
+                    context.RegisterCodeFix(
+                        CodeAction.Create("Fix interface naming", c => FixInterfaceAsync(context.Document, interfaceDeclaration, c)),
+                        diagnostic);
+                    break;
+                case MethodDeclarationSyntax methodDeclaration:
+                    context.RegisterCodeFix(
+                        CodeAction.Create("Fix method naming", c => FixMethodAsync(context.Document, methodDeclaration, c)),
+                        diagnostic);
+                    break;
+                case PropertyDeclarationSyntax propertyDeclaration:
+                    context.RegisterCodeFix(
+                        CodeAction.Create("Fix property naming", c => FixPropertyAsync(context.Document, propertyDeclaration, c)),
+                        diagnostic);
+                    break;
+                case VariableDeclaratorSyntax variableDeclarator:
+                    context.RegisterCodeFix(
+                        CodeAction.Create("Fix field naming", c => FixFieldAsync(context.Document, variableDeclarator, c)),
+                        diagnostic);
+                    break;
+            }
+        }
+
+        private async Task<Document> FixNamespaceAsync(Document document, NamespaceDeclarationSyntax namespaceDeclaration, CancellationToken cancellationToken)
+        {
+            var newName = char.ToUpper(namespaceDeclaration.Name.ToString()[0]) + namespaceDeclaration.Name.ToString().Substring(1);
+            var newNamespaceDeclaration = namespaceDeclaration.WithName(SyntaxFactory.ParseName(newName));
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(namespaceDeclaration, newNamespaceDeclaration);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        private async Task<Document> FixClassAsync(Document document, ClassDeclarationSyntax classDeclaration, CancellationToken cancellationToken)
+        {
+            var newName = char.ToUpper(classDeclaration.Identifier.ValueText[0]) + classDeclaration.Identifier.ValueText.Substring(1);
+            var newClassDeclaration = classDeclaration.WithIdentifier(SyntaxFactory.Identifier(newName));
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(classDeclaration, newClassDeclaration);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        private async Task<Document> FixInterfaceAsync(Document document, InterfaceDeclarationSyntax interfaceDeclaration, CancellationToken cancellationToken)
+        {
+            var newName = "I" + char.ToUpper(interfaceDeclaration.Identifier.ValueText[0]) + interfaceDeclaration.Identifier.ValueText.Substring(1);
+            var newInterfaceDeclaration = interfaceDeclaration.WithIdentifier(SyntaxFactory.Identifier(newName));
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(interfaceDeclaration, newInterfaceDeclaration);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        private async Task<Document> FixMethodAsync(Document document, MethodDeclarationSyntax methodDeclaration, CancellationToken cancellationToken)
+        {
+            var newName = char.ToUpper(methodDeclaration.Identifier.ValueText[0]) + methodDeclaration.Identifier.ValueText.Substring(1);
+            var newMethodDeclaration = methodDeclaration.WithIdentifier(SyntaxFactory.Identifier(newName));
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(methodDeclaration, newMethodDeclaration);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        private async Task<Document> FixPropertyAsync(Document document, PropertyDeclarationSyntax propertyDeclaration, CancellationToken cancellationToken)
+        {
+            var newName = char.ToUpper(propertyDeclaration.Identifier.ValueText[0]) + propertyDeclaration.Identifier.ValueText.Substring(1);
+            var newPropertyDeclaration = propertyDeclaration.WithIdentifier(SyntaxFactory.Identifier(newName));
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(propertyDeclaration, newPropertyDeclaration);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        private async Task<Document> FixFieldAsync(Document document, VariableDeclaratorSyntax variableDeclarator, CancellationToken cancellationToken)
+        {
+            var newName = "_" + variableDeclarator.Identifier.ValueText;
+            var newVariableDeclarator = variableDeclarator.WithIdentifier(SyntaxFactory.Identifier(newName));
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(variableDeclarator, newVariableDeclarator);
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
---
+++ Hospogate/Hospogate/HospogateAnalyzer.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+
+namespace Hospogate
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class HospogateAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "HospogateAnalyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNamespace, SyntaxKind.NamespaceDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeClass, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeInterface, SyntaxKind.InterfaceDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeProperty, SyntaxKind.PropertyDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeField, SyntaxKind.FieldDeclaration);
+        }
+
+        private void AnalyzeNamespace(SyntaxNodeAnalysisContext context)
+        {
+            var namespaceDeclaration = (NamespaceDeclarationSyntax)context.Node;
+            if (!char.IsUpper(namespaceDeclaration.Name.ToString()[0]))
+            {
+                var diagnostic = Diagnostic.Create(Rule, namespaceDeclaration.Name.GetLocation(), "Namespace should start with an uppercase letter.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeClass(SyntaxNodeAnalysisContext context)
+        {
+            var classDeclaration = (ClassDeclarationSyntax)context.Node;
+            if (!char.IsUpper(classDeclaration.Identifier.ValueText[0]))
+            {
+                var diagnostic = Diagnostic.Create(Rule, classDeclaration.Identifier.GetLocation(), "Class should start with an uppercase letter.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeInterface(SyntaxNodeAnalysisContext context)
+        {
+            var interfaceDeclaration = (InterfaceDeclarationSyntax)context.Node;
+            if (!interfaceDeclaration.Identifier.ValueText.StartsWith("I", StringComparison.Ordinal) || !char.IsUpper(interfaceDeclaration.Identifier.ValueText[1]))
+            {
+                var diagnostic = Diagnostic.Create(Rule, interfaceDeclaration.Identifier.GetLocation(), "Interface should start with 'I' followed by an uppercase letter.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+            if (!char.IsUpper(methodDeclaration.Identifier.ValueText[0]))
+            {
+                var diagnostic = Diagnostic.Create(Rule, methodDeclaration.Identifier.GetLocation(), "Method should start with an uppercase letter.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeProperty(SyntaxNodeAnalysisContext context)
+        {
+            var propertyDeclaration = (PropertyDeclarationSyntax)context.Node;
+            if (!char.IsUpper(propertyDeclaration.Identifier.ValueText[0]))
+            {
+                var diagnostic = Diagnostic.Create(Rule, propertyDeclaration.Identifier.GetLocation(), "Property should start with an uppercase letter.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeField(SyntaxNodeAnalysisContext context)
+        {
+            var fieldDeclaration = (FieldDeclarationSyntax)context.Node;
+            foreach (var variable in fieldDeclaration.Declaration.Variables)
+            {
+                if (!char.IsLower(variable.Identifier.ValueText[0]))
+                {
+                    // Check if the field is a dependency injection field
+                    var isDependencyInjectionField = fieldDeclaration.Modifiers.Any(SyntaxKind.PrivateKeyword)
+                                                     && fieldDeclaration.Modifiers.Any(SyntaxKind.ReadOnlyKeyword)
+                                                     && fieldDeclaration.Declaration.Type is SimpleNameSyntax simpleNameSyntax
+                                                     && simpleNameSyntax.Identifier.ValueText.StartsWith("I", StringComparison.Ordinal);
+
+                    // If it's a dependency injection field, check if it starts with '_'
+                    if (isDependencyInjectionField && !variable.Identifier.ValueText.StartsWith("_", StringComparison.Ordinal))
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, variable.Identifier.GetLocation(), "Dependency injection field should start with '_' followed by a lowercase letter.");
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+            }
+        }
+    }
+}
